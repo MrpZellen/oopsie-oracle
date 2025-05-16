8f80d9534f62868d95ddfd10f395df077a312e3e
Dami <68552016+xxkhione@users.noreply.github.com>
2025-05-15 19:41:59 -0600

    Finished the input logic/event handling
    Yippee!

--- pages/oracle.vue
+++ pages/oracle.vue
@@ -1,6 +1,31 @@
 <template>
   <section>
-    <input type="text" />
-    <button>Submit Question</button>
+    <div v-if="submittedText.length > 0">
+      <p v-for="(submission, index) in submittedText" :key="index">{{ submission }}</p>
+    </div>
+    <input type="text" v-model="inputText" @keyup.enter="submit"/>
+    <button @click="submit">Submit Question</button>
   </section>
 </template>
+
+<script>
+  import {ref} from 'vue'
+  
+  export default {
+    setup() {
+      const inputText = ref('')
+      const submittedText = ref([])
+      
+      function submit() {
+        submittedText.value.push(inputText.value.trim())
+        inputText.value = ''
+      }
+
+      return {
+        inputText,
+        submittedText,
+        submit
+      }
+    }
+  }
+</script>
