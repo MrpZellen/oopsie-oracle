a0a27e92e985182e2afdea8054d25e03a438185f
Dami <68552016+xxkhione@users.noreply.github.com>
2025-05-15 19:16:54 -0600

    Very Basic Front-End
    Implemented the basics, no details on any pages yet.

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/oracle">Oracle</NuxtLink></li>
+        <li><NuxtLink to="/discussions">Past Discussions</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
