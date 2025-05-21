05b10a82464b27bf10745ff4c1a3ff00f2cd7a70
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2025-05-20 19:20:06 -0600

    bye bye dummy data

--- knowledgeStorage/intents.js
+++
@@ -1,8 +0,0 @@
-export const intents = {
- greeting: ['hello', 'hi', 'hey', 'good morning', 'good evening', 'howdy'],
- goodbye: ['bye', 'goodbye', 'see you later', 'farewell', 'catch you later'],
- thanks: ['thank you', 'thanks', 'much appreciated', 'thank you very much'],
-    help: ['help', 'assist', 'support', 'can you help me', 'need assistance'],
-    question: ['question', 'query', 'inquiry', 'ask', 'wondering'],
-    
-};
