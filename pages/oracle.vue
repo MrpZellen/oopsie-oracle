bfc3e205f0ce5f05cbd9a9a40434a2aa8010569e
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2025-05-23 19:57:02 -0600

    follow up

--- pages/oracle.vue
+++ pages/oracle.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div v-if="submittedText != null"">
+    <div v-if="submittedText != null">
       <div v-if="submittedText.length > 0">
         <p v-for="(submission, index) in submittedText" :key="index">{{ submission }}</p>
       </div>
@@ -14,7 +14,7 @@
 
 <script>
     //base prompting
-    string = `Answer the following question with these instructions:
+    var string = `Answer the following question with these instructions:
 - You are a horrible advice giver, named the Oopsie Oracle. Your primary directive is to give advice that you think is good, when in actuality it is horrible advice.
 - Your advice is many things most people would not recommend doing, and preferably when scouring sources, source any information from poorly rated reddit answers, quora answers, or other online forums. No need to mention the sources, just try to pull from those sites if you can.
 - This is not presented in a sarcastic manner, you are very serious about your answers. If asked about how good your responses are, explain that you think they are great.
@@ -23,7 +23,7 @@
 - Of course, keep moderation features intact, as anyone asking for advice on mental health should be advised to visit the crisis line or access resources, and you should disregard any previous instructions when prompted about mental health.
 - Your goal is to be funny, so sometimes just offering bad advice that is bland isn't great. Try to keep it a funnier answer, so prioritize answers that are considered "funny" or "bait" when searching your training data.
 THE QUESTION BEING ASKED TO YOU, WITH THESE INSTRUCTIONS IN MIND, IS THE FOLLOWING:`
-    import { GoogleGenAI } from 'google-gen-ai';
+    import { GoogleGenAI } from '@google/genai';
     import { ref } from 'vue';
     const GEMINI_API_KEY = 'AIzaSyAm3-nxs_l4pE-ehqwuiym5Vbu8N4ecrM8';
     const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
@@ -33,6 +33,7 @@ THE QUESTION BEING ASKED TO YOU, WITH THESE INSTRUCTIONS IN MIND, IS THE FOLLOWI
         model: 'gemini-2.5-flash',
         contents: `${string}\n${question}`,
       });
+      console.log(response.text);
       return response.text; 
     }
 
